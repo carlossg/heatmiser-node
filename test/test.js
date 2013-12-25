5278b64c7726ee7f3038228ebe8e73f27a347572
Carlos Sanchez <csanchez@maestrodev.com>
2013-12-25 14:35:45 +0100

    Add a write method

--- test/test.js
+++ test/test.js
@@ -10,6 +10,38 @@ describe('heatmiser', function(){
   // pin in hex little endian: 0xd204
   var hm = new Heatmiser('localhost', 1234);
 
+  // common functions
+  var success = function(data) { }
+  var error = function(error) { throw error; }
+
+  // stub sockets
+  var stub = null;
+  var socket = null;
+
+  beforeEach(function(){
+    socket = new net.Socket({});   
+    sinon.stub(socket, 'setTimeout');
+
+    socket.on('data', function(data) {
+      // data.should.eql('foo');
+      // test is done or it will timeout and fail
+      // done();
+    });
+
+    // Stub the net.connect function
+    sinon.stub(net, 'connect');
+    // Asynchronously call the second argument with a null error and some text when passed certain arguments
+    net.connect.callsArgWithAsync(1).returns(socket);
+
+    hm.model = 'PRT-E';
+  })
+
+  // When done make sure you restore stubs to the original functionality.
+  afterEach(function(){
+    net.connect.restore();
+    if (stub != null) stub.restore();    
+  })
+
   describe('#Heatmiser()', function(){
 
     it('should return host, port and pin', function(){
@@ -24,42 +56,97 @@ describe('heatmiser', function(){
 
     it('should read the thermostat', function(done){
 
-      var socket = new net.Socket({});
-   
-      var stub = sinon.stub(socket, 'write', function (data, encoding, cb) {
+      stub = sinon.stub(socket, 'write', function (data, encoding, cb) {
 
-        try {
-          // operation: 0x93
-          // pin: 0xd204
-          // data length: 0x0b00
-          // data: 0x0000ffff
-          // checksum: 0x28b4
-          expect(data.toString('hex')).to.be("930b00d2040000ffff28b4");
-          done();
-        } finally {
-          // When done make sure you restore stubs to the original functionality.
-          net.connect.restore();
-          stub.restore();
-        }
-      });
-      sinon.stub(socket, 'setTimeout');
-
-      socket.on('data', function(data) {
-        // data.should.eql('foo');
-        // test is done or it will timeout and fail
-        // done();
-      });
-
-      // Stub the net.connect function
-      sinon.stub(net, 'connect');
-      // Asynchronously call the second argument with a null error and some text when passed certain arguments
-      net.connect.callsArgWithAsync(1).returns(socket);
-
-      hm.read_device(function(data) {
+        // operation: 0x93
+        // data length: 0x0b00
+        // pin: 0xd204
+        // data: 0x0000ffff
+        // checksum: 0x28b4
+        expect(data.toString('hex')).to.be("930b00d2040000ffff28b4");
         done();
-      }, function(error) {
-        throw error;
       });
+
+      hm.read_device(success, error);
+    })
+
+  });
+
+
+  describe('#write_device()', function(){
+
+    it('should set the thermostat time', function(done){
+
+      stub = sinon.stub(socket, 'write', function (data, encoding, cb) {
+
+        // operation: 0xA3
+        // data length: 0x1200
+        // pin: 0xD204
+        // items: 0x01
+        // data => position: 0x2B00 size: 0x07 data: 0x0D0C1903132601 (13-12-25 wed 19:38:01)
+        // checksum: 0x3E1C
+        expect(data.toString('hex')).to.be("A31200D204012B00070D0C19031326013E1C".toLowerCase());
+        done();
+      });
+
+      var dcb = {
+        time: new Date(2013,11,25,19,38,01)
+      }
+
+      hm.write_device(dcb, success, error);
+    })
+
+    it('should set the thermostat to frost mode (away)', function(done){
+
+      stub = sinon.stub(socket, 'write', function (data, encoding, cb) {
+
+        // operation: 0xA3
+        // data length: 0x0C00
+        // pin: 0xD204
+        // items: 0x01
+        // data => position: 0x1700 size: 0x01 data: 0x01 (frost)
+        // checksum: 0x38DC
+        expect(data.toString('hex')).to.be("A30C00D204011700010138DC".toLowerCase());
+
+        done();
+      });
+
+      var dcb = {
+        runmode: 'frost'
+      }
+
+      hm.write_device(dcb, success, error);
+    })
+
+    it('should set the thermostat to temperature hold and limit floor', function(done){
+
+      stub = sinon.stub(socket, 'write', function (data, encoding, cb) {
+
+        // operation: 0xA3
+        // data length: 0x1500
+        // pin: 0xD204
+        // items: 0x03
+        // data => 
+        //   position: 0x1200 size: 0x01 data: 0x14 (20C)
+        //   position: 0x2000 size: 0x02 data: 0x1E00 (30 minutes)
+        //   position: 0x1300 size: 0x01 data: 0x17 (23 C)
+        // checksum: 0x6CC1
+        expect(data.toString('hex')).to.be("A31500D20403120001142000021E00130001176CC1".toLowerCase());
+
+        done();
+      });
+
+      var dcb = {
+        heating: {
+          target: 20, // C
+          hold: 30 // minutes
+        },
+        floorlimit: {
+          floormax: 23
+        }
+      }
+
+      hm.write_device(dcb, success, error);
     })
 
   });
